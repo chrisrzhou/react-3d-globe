8349df057e398aeb286b13e9a3d1225a0546e9bc
Chang Liu <changfb@fb.com>
2018-05-10 15:02:49 -0700

    support projections

--- src/lib/Globe.js
+++ src/lib/Globe.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import OrbitControls from 'three-orbitcontrols';
-
+import {latLongToVector} from './projections';
 import Marker from './Marker';
 import {
   cameraOptions,
@@ -79,7 +79,6 @@ class Globe {
     // create renderer
     const renderer = new THREE.WebGLRenderer(rendererOptions);
     renderer.setSize(radius, radius);
-
     // bind class variables
     this.renderer = renderer;
     this.camera = camera;
@@ -88,6 +87,42 @@ class Globe {
     this.globe = globe;
     this.mouseX = window.innerWidth / 2;
     this.mouseY = window.innerHeight / 2;
+    this.frameId = null;
+  }
+
+  addMarkers(markers) {
+    // the geometry that will contain all our cubes
+    const geom = new THREE.Geometry();
+    const cubeMat = new THREE.MeshLambertMaterial({
+      color: 0x000000,
+      opacity: 0.6,
+      emissive: 0xffffff,
+    });
+    markers.forEach(marker => {
+      const position = latLongToVector(marker.lat, marker.long, 600, 2);
+      let cube = new THREE.Mesh(
+        new THREE.CubeGeometry(5, 5, 1 + marker.value / 8, 1, 1, 1, cubeMat),
+      );
+      cube.position.set(position);
+      cube.lookAt(new THREE.Vector3(0, 0, 0));
+      THREE.GeometryUtils.merge(geom, cube);
+    });
+
+    // create a new mesh, containing all the other meshes.
+    const total = new THREE.Mesh(geom, new THREE.MeshFaceMaterial());
+    this.scene.add(total);
+  }
+
+  render() {
+    this.controls.update();
+    this.renderer.render(this.scene, this.camera);
+    this.frameId = window.requestAnimationFrame(this.render.bind(this));
+  }
+
+  stop() {
+    if (this.frameId) {
+      cancelAnimationFrame(this.frameId);
+    }
   }
 }
 
